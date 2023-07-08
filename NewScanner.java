999bdaa81a71a417d049637f5205d98d005bc4d1
Adam Jaffery <adamjaffery4@gmail.com>
2023-07-08 17:35:30 -0500

    Add NewScanner -Adam

---
+++ NewScanner.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NewScanner {
+    public static void main(String[] args) {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Please enter a file path: ");
+        String filePath = scanner.nextLine();
+
+        File file = new File(filePath);
+        ArrayList<String> fileData = new ArrayList<>();
+        ArrayList<String> noiseData = new ArrayList<>();
+
+        System.out.println("Trying to read from that file...");
+        try {
+//            This doesn't have to be a Scanner, it could also be a BufferedReader or FileReader.
+//            Another way to get all the text in the file is to use Files.readAllLines
+            java.util.Scanner fileScanner = new java.util.Scanner(file);
+            while (fileScanner.hasNextLine()){
+                String data = fileScanner.nextLine();
+                fileData.add(data);
+            }
+            fileScanner.close();
+            for (int i = 0; i<fileData.size(); i++){
+                String x = fileData.get(i);
+                if (x.length()>4){
+                    noiseData.add(x);
+                    fileData.remove(i);
+                    i--;
+                }
+            }
+            //------------------------------------------GoodData Sorting------------------------------------------------
+            HashMap<Integer, Integer> valMaxMinGD = new HashMap<>();
+            for (int i = 0; i < fileData.size(); i++){
+                String X = fileData.get(i);
+                int x = Integer.parseInt(X);
+                if (valMaxMinGD.containsKey(x)){
+                    int y = valMaxMinGD.get(x) + 1;
+                    valMaxMinGD.put(x , y);
+                }
+                else {
+                    valMaxMinGD.put(x,1);
+                }
+
+            }
+            int maxKey=0;
+            int maxVal=0;
+            int minKey=0;
+            int minVal=3000;
+            for (int i : valMaxMinGD.keySet()) {
+                int hold = valMaxMinGD.get(i);
+                if(maxVal<hold){
+                    maxVal=hold;
+                    maxKey=i;
+                }
+                else if (minVal>hold){
+                    minVal=hold;
+                    minKey=i;
+                }
+            }
+            System.out.println("Most Frequent` GOOD Value: " +maxKey +". " +"Number of Reps: " +maxVal +".");
+            System.out.println("Least Frequent GOOD Value: " +minKey +". " +"Number of Reps: " +minVal +".");
+            //------------------------------------------NoiseData Sorting-----------------------------------------------
+            int minNoise = 10000000;
+            int maxNoise =0;
+            for (int i =0; i< noiseData.size(); i++){
+                String HOLD = noiseData.get(i);
+                int hold = Integer.parseInt(HOLD);
+                if(maxNoise < hold) {
+                    maxNoise = hold;
+                }
+                else if (minNoise > hold){
+                    minNoise = hold;
+                }
+            }
+            System.out.println("Largest NOISE Value: " +maxNoise);
+            System.out.println("Smallest NOISE Value: " +minNoise);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Error trying to read the file: " +e);
+        }
+
+
+
+
+    }
+}
+// C:\Users\Adam\repos\MidTermJavaChallenge\Test.txt
--- out/production/MidTermJavaChallenge/README.md
+++ out/production/MidTermJavaChallenge/README.md
@@ -11,34 +11,14 @@ Your program needs to address the following questions:
 2. Good data: Which value appears least frequently, and how many times does it repeat?
 3. Noise data: What is the highest value among the noise?
 4. Noise data: What is the lowest value among the noise?
-   Your code must be optimized for efficient memory usage and CPU utilization. This means ensuring
-   that data is stored efficiently in data structures and minimizing the number of times data is processed
-   for improved CPU efficiency
-   I have attached three sample datasets for you to test your code. Each sample file is 5MB (!!)
-   Basic rules :
-1. This assignment is to be completed as a team, with four teams selected.
-2. You are not allowed to seek assistance from individuals outside of your team to find the solution.
-3. Usage of Chat-GPT or any other external resources to aid in finding the solution is prohibited.
-4. Submit your final code to Coach Shaphy via email at shaphy@gmail.com or through Discord by
-   June 24th, Saturday 11:59 pm
-5. We will review your answers in the lab 12:00 pm next Sunday
 
-You have been provided with a Java coding assignment that requires you to process a dataset
-consisting of approx. 1 million records. The data spans from 2001 to 2999, and it contains repeated
-values (on average 1000 times). The data is primarily sorted in ascending order, but occasional
-random noise disrupts this pattern, and noise ratio is once in 500 records. Your task is to efficiently
-store the good data (ranging from 2000 to 2999) and separate out the noise into a different data
-structure.
-Your program needs to address the following questions:
-1. Good data: Which value appears most frequently, and how many times does it repeat?
-2. Good data: Which value appears least frequently, and how many times does it repeat?
-3. Noise data: What is the highest value among the noise?
-4. Noise data: What is the lowest value among the noise?
+
    Your code must be optimized for efficient memory usage and CPU utilization. This means ensuring
    that data is stored efficiently in data structures and minimizing the number of times data is processed
    for improved CPU efficiency
    I have attached three sample datasets for you to test your code. Each sample file is 5MB (!!)
-   Basic rules :
+   
+Basic rules :
 1. This assignment is to be completed as a team, with four teams selected.
 2. You are not allowed to seek assistance from individuals outside of your team to find the solution.
 3. Usage of Chat-GPT or any other external resources to aid in finding the solution is prohibited.
