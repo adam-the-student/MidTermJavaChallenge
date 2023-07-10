9fa168589289af78b724e59ac4ed1e85881fe6e2
Farid <farid.samana26@gmail.com>
2023-07-10 14:27:46 -0500

    Updated NewScanner; still need to weed out some bugs.

--- NewScanner.java
+++ NewScanner.java
@@ -1,76 +1,60 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class NewScanner {
     public static void main(String[] args) {
 
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter a file path: ");
         String filePath = scanner.nextLine();
 
         File file = new File(filePath);
-        ArrayList<String> fileData = new ArrayList<>();
-        ArrayList<String> noiseData = new ArrayList<>();
-
+        short currData=0;
+        short maxNoise = 0;
+        short minNoise = Short.MAX_VALUE;
+        short counter = 0;
+        short maxRep=0;
+        short minRep=0;
+        short minRepCounter=0;
+        short maxRepCounter=0;
         System.out.println("Trying to read from that file...");
+        scanner.close();
         try {
-            java.util.Scanner fileScanner = new java.util.Scanner(file);
-            while (fileScanner.hasNextLine()) {
-                String data = fileScanner.nextLine();
-                if (data.length() > 4) {
-                    noiseData.add(data);
-                } else {
-                    fileData.add(data);
+             Scanner fileScanner = new Scanner(file);
+
+            while (fileScanner.hasNextShort()) {
+                short data = fileScanner.nextShort();
+
+                if (data < 3000) {
+                    if (currData == data) {
+                        counter++;
+                    } else if (counter <= minRepCounter && currData > 2000) {
+                        currData = data;
+                        minRepCounter = counter;
+                        minRep = currData;
+                        counter = 1;
+                    }
+                    else if (counter >= maxRepCounter) {
+                        currData = data;
+                        maxRepCounter = counter;
+                        maxRep = currData;
+                        counter = 1;
+                    }
+                } else if (data > maxNoise) {
+                    maxNoise = data;
+                } else if (data < minNoise) {
+                    minNoise = data;
                 }
+
             }
             fileScanner.close();
-
-            //------------------------------------------GoodData Sorting------------------------------------------------
-            HashMap<Integer, Integer> valMaxMinGD = new HashMap<>();
-            for (int i = 0; i < fileData.size(); i++) {
-                String X = fileData.get(i);
-                int x = Integer.parseInt(X);
-                if (valMaxMinGD.containsKey(x)) {
-                    int y = valMaxMinGD.get(x) + 1;
-                    valMaxMinGD.put(x, y);
-                } else {
-                    valMaxMinGD.put(x, 1);
-                }
-            }
-            int maxKey = 0;
-            int maxVal = 0;
-            int minKey = 0;
-            int minVal = 3000;
-            for (int i : valMaxMinGD.keySet()) {
-                int hold = valMaxMinGD.get(i);
-                if (maxVal < hold) {
-                    maxVal = hold;
-                    maxKey = i;
-                } else if (minVal > hold) {
-                    minVal = hold;
-                    minKey = i;
-                }
-            }
-            System.out.println("Most Frequent GOOD Value: " + maxKey + ". Number of Reps: " + maxVal + ".");
-            System.out.println("Least Frequent GOOD Value: " + minKey + ". Number of Reps: " + minVal + ".");
-
-            //------------------------------------------NoiseData Sorting-----------------------------------------------
-            int minNoise = Integer.MAX_VALUE;
-            int maxNoise = Integer.MIN_VALUE;
-            for (int i = 0; i < noiseData.size(); i++) {
-                String HOLD = noiseData.get(i);
-                int hold = Integer.parseInt(HOLD);
-                if (maxNoise < hold) {
-                    maxNoise = hold;
-                } else if (minNoise > hold) {
-                    minNoise = hold;
-                }
-            }
+            System.out.println("Most Frequent GOOD Value: " + maxRep + ". " + "Number of Reps: " + maxRepCounter + ".");
+            System.out.println("Least Frequent GOOD Value: " + minRep + ". " + "Number of Reps: " + minRepCounter + ".");
             System.out.println("Largest NOISE Value: " + maxNoise);
             System.out.println("Smallest NOISE Value: " + minNoise);
-        } catch (FileNotFoundException e) {
-            System.out.println("Error trying to read the file: " + e);
+        } catch (Exception e) {
+            System.out.println("Error trying to read the file: " +e);
         }
     }
+
 }
